03180bebb32987220bf03eebdaa39bffc2d22b81
Motasim <MotaSay@gmail.com>
2015-10-20 11:41:33 +0100

    modified the mock data structure

--- microservices/api_mock/web.py
+++ microservices/api_mock/web.py
@@ -7,41 +7,41 @@ app = Flask(__name__)
 @cross_origin()
 def get_questions():
     userName = request.args.get('userName', '')
-    questions = [{
-        "answers": [
-            {"0":"Not much, really."},
-                {"1":"Oh, nothing interesting"},
-                {"2":"Just wanted to say hi."},
-                {"3":"There's no 4th answer."}
+    questions = [
+        {
+            "answers": [
+                "Not much, really.",
+                "Oh, nothing interesting",
+                "Just wanted to say hi.",
+                "There's no 4th answer."
             ],
             "question_id": 1,
             "question_str": "What's up?",
             "correct_answer":"0"
-            },
-            {
+        },
+        {
             "answers": [
-                {"0":"Man U."},
-                    {"1":"Barcelona."},
-                    {"2":"Bayern Munich."},
-                    {"3":"Liverpool."}
-                ],
-                "question_id": 2,
-                "question_str": "What's your favorite team?",
-                "correct_answer":"1"
-                },
-            {
-                "answers": [
-                    {"0","Dublin."},
-                    {"1","Galway."},
-                    {"2","Donegal."},
-                    {"3","Belfast."}
-                ],
-                "question_id": 2,
-                "question_str": "What's your favorite city?",
-                "correct_answer":"1"
-                }
-            ]
-
+                "Man U.",
+                "Barcelona.",
+                "Bayern Munich.",
+                "Liverpool."
+            ],
+            "question_id": 2,
+            "question_str": "What's your favorite team?",
+            "correct_answer":"1"
+        },
+        {
+            "answers": [
+                "Dublin.",
+                "Galway.",
+                "Donegal.",
+                "Belfast."
+            ],
+            "question_id": 2,
+            "question_str": "What's your favorite city?",
+            "correct_answer":"1"
+        }
+    ]
 
     return jsonify({"userName": userName, "questions": questions})
 
